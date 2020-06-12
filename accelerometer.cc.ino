e6e787124232ae458692ce06f636877af95dbe8e
akankshaprasad8 <52112581+akankshaprasad8@users.noreply.github.com>
2020-06-12 19:21:08 +0530

    Add files via upload

---
+++ accelerometer.cc.ino
@@ -0,0 +1,132 @@
+#include <Wire.h>
+#include <SD.h>
+#include <SPI.h>
+long accelX, accelY, accelZ;
+float gForceX, gForceY, gForceZ;
+long gyroX, gyroY, gyroZ;
+float rotX, rotY, rotZ;
+int pinCS = 4;
+File myFile;
+void setup() {
+  Serial.begin(9600); 
+  pinMode(pinCS, OUTPUT);
+  
+  // SD Card Initialization
+  if (SD.begin(pinCS))
+  {
+    Serial.println("SD card is ready to use.");
+  } else
+  {
+    Serial.println("SD card initialization failed");
+    return;
+  Serial.begin(9600);
+  Wire.begin();
+  setupMPU();
+  }
+   Serial.begin(9600);
+  Wire.begin();
+  setupMPU();
+}
+void setupMPU() {           
+  Wire.beginTransmission(0b1101000); //This is the I2C address of the MPU (b1101000/b1101001 for AC0 low/high datasheet sec. 9.2)
+  Wire.write(0x6B); //Accessing the register 6B - Power Management (Sec. 4.28)
+  Wire.write(0b00000000); //Setting SLEEP register to 0. (Required; see Note on p. 9)
+  Wire.endTransmission();
+  Wire.beginTransmission(0b1101000); //I2C address of the MPU
+  Wire.write(0x1B); //Accessing the register 1B - Gyroscope Configuration (Sec. 4.4)
+  Wire.write(0x00000000); //Setting the gyro to full scale +/- 250deg./s
+  Wire.endTransmission();
+  Wire.beginTransmission(0b1101000); //I2C address of the MPU
+  Wire.write(0x1C); //Accessing the register 1C - Acccelerometer Configuration (Sec. 4.5)
+  Wire.write(0b00001100); //Setting the accel to +/- 16g
+  Wire.endTransmission();
+}
+void loop() {
+  recordAccelRegisters();
+  recordGyroRegisters();
+  printData();
+  delay(1000);
+    myFile = SD.open("acctest.txt", FILE_WRITE);
+  if (myFile) {    
+    myFile.print("Gyro (deg)");
+    myFile.print(" gyrox=");
+  myFile.print(gyroX );
+  myFile.print(" gyroY=");
+  myFile.print(gyroY);
+  myFile.print(" gyroZ=");
+  myFile.print(gyroZ);
+    
+myFile.print(" rotx=");
+  myFile.print(rotX);
+  myFile.print(" Y=");
+  myFile.print(rotY);
+  myFile.print(" Z=");
+  myFile.print(rotZ);
+  myFile.print(" Accel (g)");
+  myFile.print(" X=");
+  myFile.print(gForceX);
+  myFile.print(" Y=");
+  myFile.print(gForceY);
+  myFile.print(" Z=");
+  myFile.println(gForceZ);
+  myFile.close();
+  }
+ // if the file didn't open, print an error:
+  else {
+    Serial.println("error opening test.txt");
+}
+
+}
+void recordAccelRegisters() {
+  Wire.beginTransmission(0b1101000); //I2C address of the MPU
+  Wire.write(0x3B); //Starting register for Accel Readings
+  Wire.endTransmission();
+  Wire.requestFrom(0b1101000, 6); //Request Accel Registers (3B - 40)
+  while (Wire.available() < 6);
+  accelX = Wire.read() << 8 | Wire.read(); //Store first two bytes into accelX
+  accelY = Wire.read() << 8 | Wire.read(); //Store middle two bytes into accelY
+  accelZ = Wire.read() << 8 | Wire.read(); //Store last two bytes into accelZ
+  processAccelData();
+}
+void processAccelData()
+{
+  gForceX = accelX / 16384.0;
+  gForceY = accelY / 16384.0;
+  gForceZ = accelZ / 16384.0;
+}
+void recordGyroRegisters()
+{
+  Wire.beginTransmission(0b1101000); //I2C address of the MPU
+  Wire.write(0x43); //Starting register for Gyro Readings
+  Wire.endTransmission();
+  Wire.requestFrom(0b1101000, 6); //Request Gyro Registers (43 - 48)
+  while (Wire.available() < 6);
+  gyroX = Wire.read() << 8 | Wire.read(); //Store first two bytes into accelX
+  gyroY = Wire.read() << 8 | Wire.read(); //Store middle two bytes into accelY
+  gyroZ = Wire.read() << 8 | Wire.read(); //Store last two bytes into accelZ
+  processGyroData();
+}
+void processGyroData()
+{
+  rotX = gyroX / 131.0;
+  rotY = gyroY / 131.0;
+  rotZ = gyroZ / 131.0;
+}
+void printData() {
+  Serial.print("Gyro (deg)");
+  Serial.print(" X=");
+  Serial.print(rotX);
+  Serial.print(" Y=");
+  Serial.print(rotY);
+  Serial.print(" Z=");
+  Serial.print(rotZ);
+  Serial.print(" Accel (g)");
+  Serial.print(" X=");
+  Serial.print(gForceX);
+  Serial.print(" Y=");
+  Serial.print(gForceY);
+  Serial.print(" Z=");
+  Serial.println(gForceZ);
+}
+
+
---
+++ adxlaudiocheck.ino
@@ -0,0 +1,150 @@
+
+
+int a=1;
+int b=1;
+int c=1;
+int d=1;
+int e=1;
+int f=1;
+int GNDPin=A4;
+int VccPin=A5; 
+int xPin=A3; 
+int yPin=A2; 
+int zPin=A1; 
+long x; 
+long y; 
+long z; 
+int cmd=0x90;
+int velocity=0x45;
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(GNDPin, OUTPUT);
+  pinMode(VccPin, OUTPUT);
+  digitalWrite(GNDPin, LOW); 
+  digitalWrite(VccPin, HIGH); 
+}
+void loop()
+{
+  x = analogRead(xPin); 
+  y = analogRead(yPin); 
+  z = analogRead(zPin);
+  Serial.print("x=");
+  Serial.println(x);
+  Serial.print("y=");
+  Serial.println(y);
+  Serial.print("z=");
+  Serial.println(z);
+   if(x<340)
+   {
+      if(a==1)
+      {    
+      Serial.println("audio 1");
+       Serial.write(cmd);
+  Serial.write(0x1E);
+  Serial.write(velocity);
+     Serial.write(cmd);
+  Serial.write(0x1E);
+  Serial.write(0x00);
+       }
+      a=0;
+      b=1;
+      c=1;
+      d=1;
+      e=1;
+      f=1;
+   }
+    else if(x>400) 
+    { if(b==1)
+    {
+      Serial.println("audio 2");
+      Serial.write(cmd);
+  Serial.write(0x1F);
+  Serial.write(velocity);
+     Serial.write(cmd);
+  Serial.write(0x1F);
+  Serial.write(0x00);
+    }
+    b=0;
+    c=1;
+    a=1;
+    d=1;
+    e=1;
+    f=1;
+    }
+    else if(y>400) 
+    { if(c==1)
+      {Serial.println("audio 3");
+      Serial.write(cmd);
+  Serial.write(0x20);
+  Serial.write(velocity);
+     Serial.write(cmd);
+  Serial.write(0x20);
+  Serial.write(0x00);
+      }
+      c=0;
+      a=1;
+      b=1;
+      d=1;
+      e=1;
+      f=1; 
+    }
+    else if(y<340) 
+    { if(d==1)
+    {
+      Serial.println("audio 4");
+      Serial.write(cmd);
+  Serial.write(0x21);
+  Serial.write(velocity);
+  delay(100);
+     Serial.write(cmd);
+  Serial.write(0x21);
+  Serial.write(0x00);
+  delay(100);
+    }
+    d=0;
+    c=1;
+    b=1;
+    a=1;
+    e=1;
+    f=1;
+    }
+    else if(z<340)
+   {if(e==1)
+     {
+      Serial.println(" audio 5");
+      Serial.write(cmd);
+  Serial.write(0x22);
+  Serial.write(velocity);
+     Serial.write(cmd);
+  Serial.write(0x22);
+  Serial.write(0x00);
+     }
+   e=0;
+   d=1;
+   c=1;
+   b=1;
+   a=1;
+   f=1;
+}
+
+else
+{
+  if(f==1)
+     {
+      Serial.println(" audio 6");
+     Serial.write(cmd);
+  Serial.write(0x22);
+  Serial.write(velocity);
+     Serial.write(cmd);
+  Serial.write(0x22);
+  Serial.write(0x00);
+     }
+   f=0;
+   d=1;
+   c=1;
+   b=1;
+   a=1;
+   e=1;
+}
+}
